d349601aba8c3195a5de8d814116a8e4b3a33a2b
Gaspar <gaspardolcemascolo@gmail.com>
2021-11-26 16:34:41 -0300

    add tailwindcss

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,7 @@
 // import App from 'next/app'
 
+import "../styles/global.css";
+
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Home = () => {
   return (
     <div>
-      <h1>Hello Next.js</h1>
+      <h1 className="text-red-500">Hello Next.js</h1>
     </div>
   );
 };
