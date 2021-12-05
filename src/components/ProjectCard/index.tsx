6c2a859e50ccd33d452c27f7561bc7fd677e5f64
Gaspar <gaspardolcemascolo@gmail.com>
2021-12-05 13:04:40 -0300

    fix

--- src/components/ProjectCard/index.tsx
+++ src/components/ProjectCard/index.tsx
@@ -10,9 +10,8 @@ interface IProps {
 
 export const ProjectCard = (props: IProps) => {
   const { loading } = props;
-  const { name, image, description, url, id } = props.project;
   const descriptionText: string =
-    description.description.substr(0, 90) + "\u2026";
+    props.project?.description.description.substr(0, 90) + "\u2026";
   return (
     <React.Fragment>
       {loading ? (
@@ -22,12 +21,12 @@ export const ProjectCard = (props: IProps) => {
         />
       ) : (
         <article className="w-full sm:w-mw transition duration-500 ease-in-out transform md:hover:-translate-y-1 md:hover:scale-105 overflow-hidden">
-          <Link href={`project/${id}`}>
+          <Link href={`project/${props.project?.id}`}>
             <a>
               <figure className="overflow-hidden bg-primary bg-opacity-25 h-auto rounded-t-lg">
                 <Image
                   objectFit="cover"
-                  src={image}
+                  src={props.project?.image}
                   alt="Picture of the author"
                   width={700}
                   height={400}
@@ -37,8 +36,14 @@ export const ProjectCard = (props: IProps) => {
           </Link>
           <section className="py-3 px-4 bg-primary bg-opacity-25 border border-transparent h-auto  rounded-b-lg">
             <div className="flex items-center">
-              <h2 className="text-xl font-bold text-gray-800 mr-3">{name}</h2>
-              <a className="underline" target="_blank" href={url}>
+              <h2 className="text-xl font-bold text-gray-800 mr-3">
+                {props.project?.name}
+              </h2>
+              <a
+                className="underline"
+                target="_blank"
+                href={props.project?.url}
+              >
                 See
               </a>
             </div>
